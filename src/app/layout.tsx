d07c91cd11f5cc0940b59ba3fa09b0ac2784c4a3
Andrii <cherkasandriy1122@gmail.com>
2024-07-29 23:35:13 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,12 +26,33 @@ export default function RootLayout({ children }: ChildrenProps) {
 		<html lang="en">
 			<head>
 				<meta name="description" content={metadata.description} />
+				<script>
+					window.USER_DATA = {
+						"id": "374494",
+						"email": "yuriy.khoma@reflecto.ai",
+						"email_verified": true,
+						"first_name": "Yuriy",
+						"last_name": "Khoma",
+						"full_name": "Yuriy Khoma",
+					};
+					// For demonstration, let's log the USER_DATA to the console
+					console.log(window.USER_DATA);
+				</script>
 				<title>{metadata.title}</title>
 			</head>
 			<body className={`${darkMode ? "dark" : "light"} ${lato.className}`}>
 				<Header mode={darkMode} onClick={setDarkMode} />
 				<Providers>{children}</Providers>
 				<Footer />
+				<link rel="stylesheet" type="text/css" href="https://reflectoadmin.s3.amazonaws.com/client-widget/develop/index.css" />
+				<script type="module" src="https://reflectoadmin.s3.amazonaws.com/client-widget/develop/index.js"></script>
+				<script type="text/javascript">
+					window.addEventListener('DOMContentLoaded', function() {
+						window.ChatAi.init({
+							projectId: "125467ec-7b71-4974-a17a-270e802b6bfe"
+						});
+					});
+				</script>
 			</body>
 		</html>
 	);
